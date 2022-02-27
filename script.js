1520149c66a76ea65f64a2191e22b8bf9a0b3781
MercEzz <miteshkvirash@gmail.com>
2022-02-27 23:32:25 +0530

    -

--- script.js
+++ script.js
@@ -151,25 +151,106 @@ function mySet() {
     });
   };
 }
-var setA = new mySet();
-var setB = new mySet();
-setA.add("a");
-setB.add("b");
-setB.add("c");
-setB.add("a");
-setB.add("d");
-console.log(setA.subset(setB));
-console.log(setA.intersection(setB).values());
-console.log(setB.difference(setA).values());
+// var setA = new mySet();
+// var setB = new mySet();
+// setA.add("a");
+// setB.add("b");
+// setB.add("c");
+// setB.add("a");
+// setB.add("d");
+// console.log(setA.subset(setB));
+// console.log(setA.intersection(setB).values());
+// console.log(setB.difference(setA).values());
 
-var setC = new Set();
-var setD = new Set();
-setC.add("a");
-setD.add("b");
-setD.add("c");
-setD.add("a");
-setD.add("d");
-console.log(setD.values());
-setD.delete("a");
-console.log(setD.has("a"));
-console.log(setD.add("d"));
+// var setC = new Set();
+// var setD = new Set();
+// setC.add("a");
+// setD.add("b");
+// setD.add("c");
+// setD.add("a");
+// setD.add("d");
+// console.log(setD.values());
+// setD.delete("a");
+// console.log(setD.has("a"));
+// console.log(setD.add("d"));
+
+// Queues
+
+function Queue() {
+  collection = [];
+  this.print = function () {
+    console.log(collection);
+  };
+  this.enqueue = function (e) {
+    collection.push(e);
+  };
+  this.dequeue = function () {
+    collection.pop();
+  };
+  this.front = function () {
+    return collection[0];
+  };
+  this.size = function () {
+    return collection.length;
+  };
+  this.isEmpty = function () {
+    return collection.length === 0;
+  };
+}
+
+// let q = new Queue();
+// q.enqueue("a");
+// q.enqueue("b");
+// q.enqueue("c");
+// q.print();
+// q.dequeue();
+// console.log(q.front());
+// q.print();
+
+function PriorityQueue() {
+  let collection = [];
+  this.printCollection = function () {
+    console.log(collection);
+  };
+  this.enqueue = function (e) {
+    if (this.isEmpty()) {
+      collection.push(e);
+    } else {
+      let added = false;
+      for (let i = 0; i < collection.length; i++) {
+        // checking priorites
+        if (e[i] < collection[i][1]) {
+          collection.splice(i, 0, e);
+          added = true;
+          break;
+        }
+      }
+      if (!added) {
+        collection.push(e);
+      }
+    }
+  };
+  this.dequeue = function () {
+    let value = collection.shift();
+    return value[0];
+  };
+  this.front = function () {
+    return collection[0];
+  };
+  this.size = function () {
+    return collection.length;
+  };
+  this.isEmpty = function () {
+    return collection.length === 0;
+  };
+}
+
+var pq = new PriorityQueue();
+pq.enqueue(["Beau Carnes", 2]);
+pq.enqueue(["Quincy Larson", 3]);
+pq.enqueue(["Ewa Mitulska-Wójcik", 1]);
+pq.enqueue(["Briana Swift", 2]);
+pq.printCollection();
+pq.dequeue();
+console.log(pq.front());
+pq.printCollection();
